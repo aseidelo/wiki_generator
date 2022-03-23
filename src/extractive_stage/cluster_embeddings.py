a09fcb603895fa31bb28039febe077e0ae74608e
aseidelo <aseideloliveira@gmail.com>
2022-03-23 14:49:21 -0300

    cluster embedding consider cluster persistence

--- src/extractive_stage/cluster_embeddings.py
+++ src/extractive_stage/cluster_embeddings.py
@@ -24,30 +24,42 @@ def embed_cluster(docs, query, embeddings, model, dim_reduction_model, n_tokens=
     title_embedding = np.array([output_embeddings[0].tolist()])
     umap_input_embeddings = dim_reduction_model.fit_transform(input_embeddings) # normalize(dim_reduction_model.fit_transform(input_embeddings)) # reduce dim of input texts and normalize
     umap_title_embeddings = dim_reduction_model.fit_transform(title_embedding) # normalize(dim_reduction_model.fit_transform(title_embedding)) # reduce dim of title and normalize
-    model.fit(umap_input_embeddings) # cluster with DBSCAN
+    model.fit(umap_input_embeddings) # cluster with HDBSCAN
     point_clusters, clusters_sizes = np.unique(model.labels_, return_counts=True)
+    '''
+    # sort clusters by its proximity to title embedding
+    title_membership_vector = hdbscan.prediction.membership_vector(model, umap_title_embeddings)[0]
+    print(title_membership_vector)
+    print(len(title_membership_vector))
+    print(max(title_membership_vector))
+    sorted_clusters_ind = sorted(range(len(title_membership_vector)), key=lambda i: title_membership_vector[i], reverse=True)
+    '''
+    # sort clusters by its persistence, according to de HDBSCAN method
     sorted_clusters_ind = sorted(range(len(model.cluster_persistence_)), key=lambda i: model.cluster_persistence_[i], reverse=True)
-    print('labels', model.labels_)
-    print('point_clusters', point_clusters)
-    print('cluster sizes', clusters_sizes)
-    print('cluster persistence', model.cluster_persistence_)
+    #print(sorted_clusters_ind)
+    #print('labels', model.labels_)
+    #print('point_clusters', point_clusters)
+    #print('cluster sizes', clusters_sizes)
+    #print('cluster persistence', model.cluster_persistence_)
     #print(argmin_sent_centroid)
     #to_out_ind = sorted(range(len(point_clusters[1:])), key=lambda i: point_clusters[i], reverse=True) # sort distances
-    print('sorted cluster indexes', sorted_clusters_ind)
-    print('len sorted cluster indexes', len(sorted_clusters_ind))
-    print('len labels', len(point_clusters))
-    print('n points', len(model.labels_))
-    print('len exemplars', len(model.exemplars_))
+    #print('sorted cluster indexes', sorted_clusters_ind)
+    #print('len sorted cluster indexes', len(sorted_clusters_ind))
+    #print('len labels', len(point_clusters))
+    #print('n points', len(model.labels_))
+    #print('len exemplars', len(model.exemplars_))
+    #print(model.exemplars_)
     to_out = []
     n = 0
     for cluster in sorted_clusters_ind:
+        #print('cluster_id', cluster)
         cluster_exemplars = model.exemplars_[cluster]
-        print('cluster_id', cluster)
         #print('exemplars', cluster_exemplars)
         argmin_exemplar_title, distances_sent_centroid = pairwise_distances_argmin_min(umap_title_embeddings, cluster_exemplars, metric='euclidean')
-        print(argmin_exemplar_title)
-        #pos = np.where(umap_input_embeddings == cluster_exemplars[argmin_exemplar_title[0]])
+        #print(argmin_exemplar_title)
+        #pos = np.where(umap_input_embeddings == cluster_exemplars[argmin_exemplar_title[0]].reshape(1, -1))
         pos, dist = pairwise_distances_argmin_min(cluster_exemplars[argmin_exemplar_title[0]].reshape(1, -1), umap_input_embeddings, metric='euclidean')
+        #print(pos)
         to_out_sent_ind = pos[0]
         '''
         to_out_sent_ind = None
@@ -67,7 +79,7 @@ def embed_cluster(docs, query, embeddings, model, dim_reduction_model, n_tokens=
             if(n > n_documents):
                 break
             to_out.append(docs[to_out_sent_ind])
-    print(to_out)
+    #print(to_out)
     return to_out
 
  
@@ -123,21 +135,21 @@ def dataset_embed_cluster(input_file_path, target_file_path, output_file_path, m
                     for sent in extractive_summary:
                         extractive_summary_str = extractive_summary_str + sent.replace('\n', '') + ' </s>'
                     output_file.write('{}\n'.format(extractive_summary_str).encode('utf-8'))
-                    if(i % 1 == 0):
+                    if(i % 100 == 0):
                         new_time = time.time()
                         print("{}, {} - {:.1f} s".format(i, sample['title'], new_time - start_time))
                         start_time = new_time
                     i = i + 1
 
 if __name__ == '__main__':
-    input_file_path = "../../data/wikisum_ptbr/train_test_split/input_train.csv"
-    target_file_path = "../../data/wikisum_ptbr/train_test_split/output_train.csv"
-    embeddings_input_file_path = "../../data/extractive_stage/cluster_embeddings/inputs_bert-base-portuguese-cased_train.npy"
-    embeddings_target_file_path = "../../data/extractive_stage/cluster_embeddings/outputs_bert-base-portuguese-cased_train.npy"
-    output_file_path = "../../data/extractive_stage/cluster_embeddings/input_train.csv.embed_cluster"
+    input_file_path = "../../data/wikisum_ptbr/train_test_split/input_test.csv"
+    target_file_path = "../../data/wikisum_ptbr/train_test_split/output_test.csv"
+    embeddings_input_file_path = "../../data/extractive_stage/cluster_embeddings/inputs_bert-base-portuguese-cased_test_COMPLETE.npy"
+    embeddings_target_file_path = "../../data/extractive_stage/cluster_embeddings/outputs_bert-base-portuguese-cased_test_COMPLETE.npy"
+    output_file_path = "../../data/extractive_stage/cluster_embeddings/input_test.csv.embed_cluster"
     eps = 0.1
     min_samples = 3
     #cluster_model = DBSCAN(eps=eps, min_samples=min_samples, metric='cosine')
-    cluster_model = hdbscan.HDBSCAN(min_cluster_size=5, metric='euclidean', cluster_selection_method='leaf')
+    cluster_model = hdbscan.HDBSCAN(min_cluster_size=5, metric='euclidean', cluster_selection_method='leaf', prediction_data=True)
     dim_reduction_model = umap.UMAP(n_neighbors=5, n_components=5, metric='euclidean')
     dataset_embed_cluster(input_file_path, target_file_path, output_file_path, cluster_model, dim_reduction_model, n_tokens=1000, n_documents=None)
